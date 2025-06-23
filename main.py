ee4df3fe08189316906bc715ac9ad2119ae4281a
Hasnain <kousar@DellKousar.localdomain>
2025-06-23 14:38:07 -0400

    created a main.py to run the calculator

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+This is the main module for the Calculation class, which handles arithmetic operations
+and provides methods for performing calculations based on the specified operation type.
+"""
+
+from app.calculator_repl import run_calculator_repl
+if __name__ == "__main__":
+    run_calculator_repl()
+
+
